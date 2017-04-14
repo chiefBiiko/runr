241bc5a147bab1ce09819b26abba1297323536bc
Noah Schwarz <noah.anabiik.schwarz@gmail.com>
2017-04-14 08:38:13 +0200

    a few tweaks

--- runParallel.R
+++ runParallel.R
@@ -1,5 +1,9 @@
 # runParallel
 
+lapply(list('sys', 'jsonlite'), function(p) {
+  if (!p %in% .packages(T)) install.packages(p)
+})
+
 source('https://github.com/chiefBiiko/countMatch/raw/master/countMatch.R')
 
 runParallel <- function(tasks=list(NULL), cb=NULL) {
@@ -11,7 +15,7 @@ runParallel <- function(tasks=list(NULL), cb=NULL) {
     unlink('runParallel', recursive=T)
   })
   dir.create('runParallel')  # root for all tasks
-  lapply(1:length(tasks), function(i) {  # each task gets an own dir
+  lapply(1L:length(tasks), function(i) {  # each task gets an own dir
     dir.create(file.path('runParallel', as.character(i)))
   })
   mcall <- gsub('\\s+', ' ',  # get unevaluated inputs
@@ -23,11 +27,11 @@ runParallel <- function(tasks=list(NULL), cb=NULL) {
     sub('^.*list\\((.*)(\\),\\scb.+)|(\\)\\))$', '\\1', mcall, perl=T)
   }
   # split funcs on comma and space
-  split <- strsplit(funcs, ', ', fixed=T)[[1]]
+  split <- strsplit(funcs, ', ', fixed=T)[[1L]]
   # substitute unnamed functions with 'anonymous'
-  aames <- sub('function.*', 'anonymous', split, perl=T)
+  aames <- sub('^function.+$', 'anonymous', split, perl=T)
   # if anonymous suffix is index
-  games <- sapply(1:length(aames), function(i) {
+  games <- sapply(1L:length(aames), function(i) {
     if (aames[i] == 'anonymous') {
       paste0('anonymous', as.character(i))
     } else {
@@ -35,7 +39,7 @@ runParallel <- function(tasks=list(NULL), cb=NULL) {
     }
   })
   # make filenames
-  FLNMS_R <- lapply(1:length(tasks), function(i) {
+  FLNMS_R <- lapply(1L:length(games), function(i) {
     file.path('runParallel', 
               as.character(i), 
               paste0('xp.', games[i], '.R'))
@@ -44,10 +48,10 @@ runParallel <- function(tasks=list(NULL), cb=NULL) {
     sub('R$', 'json', n, perl=T)
   })
   # prepare input tasks
-  xp.tasks <- lapply(1:length(tasks), function(i) {
+  xp.tasks <- lapply(1L:length(games), function(i) {
     paste0('sink(file=\'',
            FLNMS_JSON[[i]],
-           '\', append=T)',
+           '\')',
            '\n',
            'jsonlite::toJSON(c((', 
            paste0(deparse(tasks[[i]]), collapse=''), 
@@ -55,31 +59,31 @@ runParallel <- function(tasks=list(NULL), cb=NULL) {
   })
   # further preparation
   PID <- list()  # memory for PIDs of tasks
-  lapply(1:length(xp.tasks), function(i) {
+  lapply(1L:length(games), function(i) {
     # export prepared tasks to their designated directory
     cat(xp.tasks[[i]], file=FLNMS_R[[i]])
     # make a json log for each xp.task
     cat('', file=FLNMS_JSON[[i]])
     # start child processes and record their pids
-    PID[games[i]] <<- sys::exec_background('Rscript', FLNMS_R[[i]], T, T)
+    PID[games[i]] <<- sys::exec_background('Rscript', FLNMS_R[[i]], F, F)
   })
   # enter blocking loop till all tasks are done
   status <- lapply(PID, function(p) F)  # task status completed: T/F
   x <- lapply(status, function(s) NULL)  # return object
-  i <- 1
+  i <- 1L
   repeat {  # preferring repeat with a counter over while
     # check if current task completed
-    if (countMatch(FLNMS_JSON[[i]], 'runParallel_END', perl=F, fixed=T) > 0) {
+    if (countMatch(FLNMS_JSON[[i]], 'runParallel_END', perl=F, fixed=T) > 0L) {
       # read in return value
-      x[games[i]] <- jsonlite::fromJSON(FLNMS_JSON[[i]])[1]
+      x[games[i]] <- jsonlite::fromJSON(FLNMS_JSON[[i]])[1L]
       # mark current task as completed
       status[games[i]] <- T
     }
     # check if all tasks have completed
     if (all(unlist(status))) break
-    i <- i + 1  # increment
-    if (i > length(xp.tasks)) i <- 1  # rewind
+    i <- i + 1L  # increment
+    if (i > length(games)) i <- 1L  # rewind
   }
   # exit
-  return(x)
+  return(if (is.function(cb)) cb(x) else x)
 }
