5915afaab88b2f390b0dc4c003560f7f06ede00d
Noah Schwarz <noah.anabiik.schwarz@gmail.com>
2017-04-15 10:35:48 +0200

    refined error handling at series and waterfall

--- runSeries.R
+++ runSeries.R
@@ -2,27 +2,29 @@
 
 # Usage:
 #   moo <- function() 'mooooooo'
-#   runSeries(list(function() 1L, function() 2L, moo), function(d, e) print(d))
+#   runSeries(list(function() 1L, function() 2L, moo), function(d, err) print(d))
 
 source('https://github.com/chiefBiiko/runr/raw/master/getFuncNames.R')
 
 runSeries <- function(tasks=list(NULL), cb=NULL) {
   stopifnot(all(sapply(tasks, function(t) is.function(t))),
-            is.null(cb) || is.function(cb))
+            is.null(cb) || is.function(cb),
+            if (is.function(cb)) length(formals(cb)) == 2L)
   # setup
   games <- getFuncNames(tasks, cb)  # returns the names of tasks only
   # call series
-  ERR <- NULL
-  x <- lapply(tasks, function(task) {
-    tryCatch(task(), 
-             error=function(e) {
-               ERR <<- e
-               return(e)
-             })
-  })
+  err <- NULL
+  x <- withRestarts(  # restarts allow breaking an apply iterator
+    lapply(tasks, function(task) {
+      tryCatch(task(),                                                # try
+               error=function(e) err <<- geterrmessage(),             # catch
+               finally=if (!is.null(err)) invokeRestart("stopLoop"))  # finally
+    }),
+    stopLoop=function() NULL  # if error break immediately and return NULL
+  )
   # set names
   if (length(games) > length(x)) games <- games[1:length(x)]
-  names(x) <- games
+  if (is.list(x)) names(x) <- games
   # returning
-  return(if (is.function(cb)) cb(x, ERR) else x)
+  return(if (is.function(cb)) cb(x, err) else x)
 }
--- runWaterfall.R
+++ runWaterfall.R
@@ -2,24 +2,31 @@
 
 # Usage:
 #   runWaterfall(list(function() 1L, function(a) a + 2L, function(a) a + 3L), 
-#                function(d) print(d))
+#                function(d, err) print(d))
 
 source('https://github.com/chiefBiiko/runr/raw/master/getFuncNames.R')
 
 runWaterfall <- function(tasks=list(NULL), cb=NULL) {
   stopifnot(all(sapply(tasks, function(t) is.function(t))),
-            is.null(cb) || is.function(cb))
+            is.null(cb) || is.function(cb),
+            if (is.function(cb)) length(formals(cb)) == 2L)
   # setup
   games <- getFuncNames(tasks, cb)  # returns the names of tasks only
   # call series as waterfall
-  x <- list()  # memory of return values
-  lapply(1L:length(games), function(i) {
-    if (i == 1L) {
-      x[games[i]] <<- tasks[[i]]()
-    } else {
-      x[games[i]] <<- tasks[[i]](x[[i - 1L]])
-    }
-  })
+  err <- NULL
+  x <- list()  # return object
+  withRestarts(  # restarts allow breaking an apply iterator
+    lapply(1L:length(games), function(i) {
+      tryCatch(x[games[i]] <<- if (i == 1L) {                         # try
+                 tasks[[i]]()  # first task does not have predecessor
+               } else {
+                 tasks[[i]](x[[i - 1L]])  # all others do
+               },
+               error=function(e) err <<- geterrmessage(),             # catch
+               finally=if (!is.null(err)) invokeRestart("stopLoop"))  # finally
+    }),
+    stopLoop=function() x <<- NULL  # if error break and set x to NULL
+  )
   # returning
-  return(if (is.function(cb)) cb(x) else x)
+  return(if (is.function(cb)) cb(x, err) else x)
 }
