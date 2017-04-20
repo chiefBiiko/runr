1bcbcb6da912d3d7d309ec6f1d599df95b63c0b2
Noah Schwarz <noah.anabiik.schwarz@gmail.com>
2017-04-20 08:21:00 +0200

    error and callback testing pending

---
+++ tests/testthat/test_naming.R
@@ -0,0 +1,34 @@
+# naming
+
+testthat::context('naming')
+
+moo <- function() 'moooo'
+doo <- function() data.frame(a=0L:9L, 
+                             b=10L:19L)
+
+testthat::test_that('naming convention is robust', {
+  # anonymous inputs only
+  testthat::expect_named(runSeries(list(function() 0L, 
+                                        function() NULL)),
+                         c('function1', 'function2'))
+  # mixed inputs
+  testthat::expect_named(runWaterfall(list(moo, 
+                                           function(a) paste0(a, 'la'))),
+                         c('function1', 'function2'))
+  # named inputs only
+  testthat::expect_named(runParallel(list(moo, 
+                                          doo)),
+                         c('moo', 'doo'))
+  # messed up anonymous inputs
+  testthat::expect_named(runParallel(list(function(a, b) 
+                                            list(a=1L, b=77L),
+                                          function() list(a=0.5564, 
+                                                          b=0.3443,
+                                                          z=FALSE),
+                                          function(x, y) {  # nonsense
+                                            Sys.sleep(7L)
+                                            return(0L)
+                                          })),
+                         c('function1', 'function2', 'function3'))
+})
+
---
+++ tests/testthat/test_return.R
@@ -0,0 +1,27 @@
+# return
+
+testthat::context('return')
+
+moo <- function() 'moooo'
+doo <- function() data.frame(a=0L:9L, 
+                             b=10L:19L)
+
+testthat::test_that('return data is valid', {
+  # case error encountered
+  testthat::expect_null(runSeries(list(moo,
+                                       function() stop('throwing for fun'))))
+  # return is named list
+  testthat::expect_identical(runWaterfall(list(function() 111L,
+                                               function(a) a + 2L)),
+                             list(function1=111L,
+                                  function2=113L))
+  # data.frame's base type is list
+  testthat::expect_type(runSeries(list(function() 111L,
+                                       doo))$function2,
+                        'list')
+  # returns from children
+  testthat::expect_identical(runParallel(list(function() F,
+                                              function() NULL)),
+                             list(function1=FALSE, 
+                                  function2=NULL))
+})
