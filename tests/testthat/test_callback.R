6256a05bc2a42dfc3d1381697f0fd801e4a99807
Noah Schwarz <noah.anabiik.schwarz@gmail.com>
2017-04-21 11:49:40 +0200

    what a call

--- tests/testthat/test_callback.R
+++ tests/testthat/test_callback.R
@@ -7,17 +7,32 @@ hoo <- function() list(17L:36L)
 callback <- function(err, d) if (is.null(err)) d else stop(err, err$task)
 
 testthat::test_that('callback has either error or data', {
-  # without any error callback's return is list ...
+  # without any error callback's return is list
   testthat::expect_type(runSeries(list(moo,
                                        hoo),
                                   callback),
                         'list')
-  # either or principle - and error NULL
+  # check callback data
+  testthat::expect_identical(runSeries(list(moo,
+                                            hoo),
+                                       callback),
+                             list(moo='moooo',
+                                  hoo=list(17L:36L)))
+  # without error error is NULL
   testthat::expect_null(runRace(list(moo, 
                                      hoo),
                                 function(err, d) list(err, d))[[1L]])
-  # error object always has a $task property
-  testthat::expect_type(runParallel(list(moo, 
+  # without any error callback's return is named
+  testthat::expect_named(runSeries(list(moo,
+                                        hoo),
+                                   callback),
+                         c('moo', 'hoo'))
+  # callback must have exactly two parameters (1st class error)
+  testthat::expect_error(runRace(list(moo,
+                                      hoo),
+                                 function(d) d))
+  # 2nd class error object always has a $task property
+  testthat::expect_type(runParallel(list(moo,  # (2nd class error)
                                          function() stop('color')),
                                     function(err, d) list(err, d))[[1L]]$task,
                         'character')
